272585487c44311197f5482954e53bda7cb1bfdd
RowanPl <Rowanplooij@hotmail.com>
2024-06-28 09:35:42 +0200

    initialize

---
+++ src/main/java/org/example/votes/VotesApplication.java
@@ -0,0 +1,13 @@
+package org.example.votes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VotesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VotesApplication.class, args);
+    }
+
+}
---
+++ src/main/java/org/example/votes/models/votes.java
@@ -0,0 +1,11 @@
+package org.example.votes.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+public class votes {
+    @Id
+    private long id;
+}
+
