556975bde188c8e664e3dcbaa3c64dc3e9237205
RowanPl <Rowanplooij@hotmail.com>
2024-09-20 09:18:38 +0200

    added an exceptionController and RecordNotFoundException

---
+++ src/main/java/org/example/votes/controller/ExceptionController.java
@@ -0,0 +1,18 @@
+package org.example.votes.controller;
+
+import org.example.votes.exception.RecordNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class ExceptionController {
+
+    @ExceptionHandler(value = RecordNotFoundException.class)
+    public ResponseEntity<Object> exception(RecordNotFoundException exception) {
+        return new ResponseEntity<>(exception.getMessage(), HttpStatus.NOT_FOUND);
+
+    }
+
+}
